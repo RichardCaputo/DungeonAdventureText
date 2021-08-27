cfbc17938ab25c53363241815c54c392ad72393a
Richard Caputo <ledforeigner@gmail.com>
2021-08-26 21:18:22 -0500

    Changes by 242924 added test for getRandomInt

--- src/test/java/org/example/EnemyTest.java
+++ src/test/java/org/example/EnemyTest.java
@@ -6,7 +6,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EnemyTest {
 
+
     @Test
-    void getRandomInt() {
+    void getRandomIntReturnsZeroWhenMinBoundIsZeroAndMaxBoundIsZero() {
+        // Arrange
+        Enemy enemy = new Enemy("","",10,10,10,false,false,false);
+
+        // Act
+        int result = enemy.getRandomInt(0,1);
+
+        // Assert
+        assertTrue(result <= 1);
+    }
+
+    @Test
+    void getRandomIntReturns5WhenMinBoundIs5AndMaxBoundIs5() {
+        // Arrange
+        Enemy enemy = new Enemy("","",10,10,10,false,false,false);
+
+        // Act
+        int result = enemy.getRandomInt(5,5);
+
+        // Assert
+        assertEquals(5, result);
     }
 }
